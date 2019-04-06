a97c6ca2d53b32794955954812f1ce71fbfbb979
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2019-04-06 17:54:11 -0600

    Initial commit

---
+++ arduino/src/main.cpp
@@ -0,0 +1,135 @@
+#include "Arduino.h"
+
+#define SHIFT_DATA 2
+#define SHIFT_CLOCK 3
+#define SHIFT_LATCH 4
+#define DATA_PIN 5 // first data pin, 7 others will follow afterwards
+#define WRITE_ENABLE 13
+#define CHIP_ENABLE A0
+#define OUTPUT_ENABLE A1
+
+void high_pulse(int pin) {
+  digitalWrite(pin, LOW);
+  digitalWrite(pin, HIGH);
+  digitalWrite(pin, LOW);
+}
+
+void low_pulse(int pin) {
+  digitalWrite(pin, HIGH);
+  digitalWrite(pin, LOW);
+  digitalWrite(pin, HIGH);
+}
+
+void set_address(int address) {
+  shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, address >> 8);
+  shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, address);
+
+  high_pulse(SHIFT_LATCH);
+}
+
+void data_read_mode() {
+  for (int i = DATA_PIN; i <= DATA_PIN + 7; i++) {
+    pinMode(i, INPUT);
+  }
+}
+
+void data_write_mode() {
+  for (int i = DATA_PIN; i <= DATA_PIN + 7; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+int read_byte() {
+  int number = 0;
+  int temp;
+  for (int i = 0; i < 8; i++) {
+    temp = (int)digitalRead(DATA_PIN + i);
+    temp = temp << i;
+    number = number | temp;
+  }
+  return number;
+}
+
+int read(int address) {
+  data_read_mode();
+  set_address(address);
+  digitalWrite(WRITE_ENABLE, HIGH);
+  digitalWrite(CHIP_ENABLE, LOW);
+  digitalWrite(OUTPUT_ENABLE, LOW);
+  delayMicroseconds(2); // address to output delay(tACC)
+
+  int read = read_byte();
+  digitalWrite(CHIP_ENABLE, HIGH);
+  return read;
+}
+
+void write_byte(int address, int byte) {
+  set_address(address);
+
+  int temp;
+  for (int i = 0; i < 8; i++) {
+    temp = byte >> i;
+    temp = temp & 1;
+    digitalWrite(DATA_PIN + i, (bool)temp);
+  }
+
+  digitalWrite(CHIP_ENABLE, LOW);
+  digitalWrite(WRITE_ENABLE, LOW);
+
+  delayMicroseconds(1); // write pulse width(TWP)
+  digitalWrite(WRITE_ENABLE, HIGH);
+  digitalWrite(CHIP_ENABLE, HIGH);
+  delayMicroseconds(1);
+}
+
+void write_prepare() {
+  data_write_mode();
+  digitalWrite(OUTPUT_ENABLE, HIGH);
+  digitalWrite(CHIP_ENABLE, HIGH);
+  digitalWrite(WRITE_ENABLE, HIGH);
+}
+
+void write(int address, int byte) {
+  write_prepare();
+
+  write_byte(address, byte);
+}
+
+/*void disable_software_protection() {
+  write_prepare();
+
+  write_byte(0x5555, 0xAA);
+  write_byte(0x2AAA, 0x55);
+  write_byte(0x5555, 0x80);
+  write_byte(0x5555, 0xAA);
+  write_byte(0x2AAA, 0x55);
+  write_byte(0x5555, 0x20);
+
+  delay(15); // wait for write cycle time (tWC)
+  digitalWrite(CHIP_ENABLE, LOW);
+  }*/
+
+void setup() {
+
+  digitalWrite(CHIP_ENABLE, HIGH);
+  digitalWrite(WRITE_ENABLE,
+               HIGH); // need to set these pins to high beforehand, otherwise
+                      // they will be pulsed low when the arduino is powered on
+  pinMode(SHIFT_DATA, OUTPUT);
+  pinMode(SHIFT_CLOCK, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
+  pinMode(WRITE_ENABLE, OUTPUT);
+  pinMode(CHIP_ENABLE, OUTPUT);
+  pinMode(OUTPUT_ENABLE, OUTPUT);
+
+  digitalWrite(CHIP_ENABLE, HIGH);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i <= 0x7FFF; i++) {
+    write(i, i % 255);
+    Serial.println(read(i));
+  }
+}
