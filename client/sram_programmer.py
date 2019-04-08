fa16d15d3f4eeb179024cf36c5a3310215405038
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2019-04-07 19:36:56 -0500

    Add file verification

--- client/sram_programmer.py
+++ client/sram_programmer.py
@@ -53,6 +53,23 @@ def write_file(args):
     print('File writing complete')
 
 
+def verify_file(args):
+    initialize_serial(args.p, args.b)
+    address = int(args.offset_address, 16)
+    with open(args.file, 'rb') as f:
+        f.seek(int(args.offset_file, 16), 1)
+        byte = f.read(1)
+        while(byte != b''):
+
+            if(not verify_byte(address, int.from_bytes(byte, byteorder='little'))):
+                print('Byte at '+str(hex(address))+' not ' +
+                      str(int.from_bytes(byte, 'little')))
+
+            address += 1
+            byte = f.read(1)
+    print('File verification complete')
+
+
 def do_write(address, data):
     arduino.write(b'w\n')
     arduino.write((str(address)+'\n'+str(data)+'\n').encode('ascii'))
@@ -85,6 +102,14 @@ def main():
         '-offset_address', help='starts writing to this sram address(default: 0x0000)', default='0x0000')
     parser_write_file.add_argument(
         '-offset_file', help='writes starting from this address from file(default:0x0000)', default='0x0000')
+
+    parser_verify_file = subparsers.add_parser('verify_file')
+    parser_verify_file.add_argument('file', help='verifies this file')
+    parser_verify_file.add_argument(
+        '-offset_address', help='starts verifying at this sram address(default: 0x0000)', default='0x0000')
+    parser_verify_file.add_argument(
+        '-offset_file', help='verifies starting from this address from file(default:0x0000)', default='0x0000')
+    parser_verify_file.set_defaults(func=verify_file)
 
     args = parser.parse_args()
     args.func(args)
